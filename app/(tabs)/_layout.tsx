7c7c0c2aac44856cdf3dc795a1e53e88aab4297b
paarthiboi <paarthtrehan2@gmail.com>
2025-02-02 13:45:42 -0500

    changes2

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,6 +17,21 @@ export default function TabLayout() {
           title: 'Settings',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
+        />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
       />
     </Tabs>
   );
