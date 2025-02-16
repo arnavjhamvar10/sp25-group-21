e8de54a74b3b0740088c100fc7fe9848893bbec5
paarthiboi <paarthtrehan2@gmail.com>
2025-02-16 02:58:41 -0500

    nnn

--- app/Components/Categories.tsx
+++ app/Components/Categories.tsx
@@ -1,50 +1,33 @@
-import{ StyleSheet, Text, View , FlatList, Pressable} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
+import CategoryCard from './CategoryCard';
 
-import { useNavigation } from '@react-navigation/native';
-
+// const img = require('../../assets/images/4.jpeg');
+interface Category {
+  _id: number,
+  imageUrl: string,
+  name: string
+}
 const Categories = () => {
-    const items = [
-        {
-            id:"1",
-            name:"fastest delivery",
-        },
-        {
-            id:"2",
-            name:"rating 4.0+"
-        },
-        {
-            id:"3",
-            name:"offers",
-        },
-        {
-            id:"4",
-            name:"cuisines",
-        },
-        {
-            id:"5",
-            name:"MAX Safety",
-        },
-        {
-            id:"6",
-            name:"Pro",
-        }
-    ]
+  const [categories, setCategories] = useState<Category[]>([]);;
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 16,
+        padding: 12,
+      }}
+    >
+      {categories.map((category) => (
+        <CategoryCard
+          key={category._id}
+          imgUrl={`${category.imageUrl}?width=200`}
+          title={category.name}
+        />
+      ))}
+    </ScrollView>
+  );
+};
 
-        return (
-          <View style={{ marginTop: 6, marginBottom: 5 }}>
-            <FlatList showsHorizontalScrollIndicator={false}
-              horizontal={true}
-              data={items}
-              renderItem={({item}) => (
-                <Pressable style ={{backgroundColor:"green",margin: 7,padding:5,borderRadius:4}}>
-                    <Text style={{color:"#FFF"}}>{item.name}</Text>
-                </Pressable>
-              )}/>
-          </View>
-        );
-    }
-
-export default Categories
-
-const styles = StyleSheet.create({})
+export default Categories;
